da76f326b001bd56b421d9e0fc66b76566615549
au.mironov <au.mironov@tensor.ru>
2021-08-24 14:40:58 +0700

    INIT

---
+++ coderoad/config-overrides.js
@@ -0,0 +1,32 @@
+/*
+Config is overwritten for the CodeRoad integration
+because of the need to change the Jest reporter on tests.
+See the README for details on how to revert back.
+*/
+
+module.exports = {
+  webpack: config => {
+    return config;
+  },
+  devServer: config => {
+    return config;
+  },
+  jest: config => ({
+    ...config,
+    // stop running tests after `n` failures
+    bail: 1,
+    // use jest-tap-reporter with CodeRoad tests
+    reporters: ["jest-tap-reporter"],
+    // disable test coverage collection for performance
+    collectCoverage: false,
+    collectCoverageFrom: [],
+    // add a test setup file if need
+    setupFilesAfterEnv: ["./test/setupTests.js"],
+    // unused fields with CodeRoad
+    watchPlugins: [],
+    // roots: ['./test']
+  }),
+  paths(paths, env) {
+    return paths;
+  }
+};
